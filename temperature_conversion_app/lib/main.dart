44855b6ccf6a69bb88d8e7e2453ab03595b2c367
Pierrette Umutoniwase <p.umutoniwa@alustudent.com>
2024-07-15 18:57:34 +0200

    Made some UI changes

--- temperature_conversion_app/lib/main.dart
+++ temperature_conversion_app/lib/main.dart
@@ -64,120 +64,117 @@ class _TemperatureConversionScreenState
       appBar: AppBar(
         title: Text('Temperature Conversion'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            _buildResultSection(),
-            SizedBox(height: 20),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('Enter Temperature',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  SizedBox(height: 10),
-                  Row(
-                    children: [
-                      Expanded(
-                        flex: 3,
-                        child: TextField(
-                          controller: _controller,
-                          keyboardType: TextInputType.number,
-                          decoration: InputDecoration(
-                            labelText: 'Degree',
-                            border: OutlineInputBorder(),
-                          ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                _buildResultSection(),
+                SizedBox(height: 20),
+                Text('Enter Temperature',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      flex: 3,
+                      child: TextField(
+                        controller: _controller,
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: 'Degree',
+                          border: OutlineInputBorder(),
                         ),
                       ),
-                      SizedBox(width: 10),
-                      Expanded(
-                        flex: 1,
-                        child: DropdownButtonFormField<String>(
-                          value: _isCelsius ? 'C' : 'F',
-                          items: <DropdownMenuItem<String>>[
-                            DropdownMenuItem<String>(
-                              value: 'C',
-                              child: Text('°C'),
-                            ),
-                            DropdownMenuItem<String>(
-                              value: 'F',
-                              child: Text('°F'),
-                            ),
-                          ],
-                          onChanged: (String? value) {
-                            if (value != null) {
-                              _toggleConversion();
-                            }
-                          },
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      flex: 1,
+                      child: DropdownButtonFormField<String>(
+                        value: _isCelsius ? 'C' : 'F',
+                        items: <DropdownMenuItem<String>>[
+                          DropdownMenuItem<String>(
+                            value: 'C',
+                            child: Text('°C'),
                           ),
+                          DropdownMenuItem<String>(
+                            value: 'F',
+                            child: Text('°F'),
+                          ),
+                        ],
+                        onChanged: (String? value) {
+                          if (value != null) {
+                            _toggleConversion();
+                          }
+                        },
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
                         ),
                       ),
-                    ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text('Convert In',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                DropdownButtonFormField<String>(
+                  value: _isCelsius ? 'Fahrenheit (°F)' : 'Celsius (°C)',
+                  items: <DropdownMenuItem<String>>[
+                    DropdownMenuItem<String>(
+                      value: 'Fahrenheit (°F)',
+                      child: Text('Fahrenheit (°F)'),
+                    ),
+                    DropdownMenuItem<String>(
+                      value: 'Celsius (°C)',
+                      child: Text('Celsius (°C)'),
+                    ),
+                  ],
+                  onChanged: (String? value) {
+                    if (value != null) {
+                      _toggleConversion();
+                    }
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
                   ),
-                  SizedBox(height: 20),
-                  Text('Convert In',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  DropdownButtonFormField<String>(
-                    value: _isCelsius ? 'Fahrenheit (°F)' : 'Celsius (°C)',
-                    items: <DropdownMenuItem<String>>[
-                      DropdownMenuItem<String>(
-                        value: 'Fahrenheit (°F)',
-                        child: Text('Fahrenheit (°F)'),
-                      ),
-                      DropdownMenuItem<String>(
-                        value: 'Celsius (°C)',
-                        child: Text('Celsius (°C)'),
-                      ),
-                    ],
-                    onChanged: (String? value) {
-                      if (value != null) {
-                        _toggleConversion();
-                      }
+                ),
+                SizedBox(height: 20),
+                Center(
+                  child: ElevatedButton(
+                    onPressed: _convertTemperature,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromARGB(255, 109, 204, 195),
+                      padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                      textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    child: Text('Convert'),
+                  ),
+                ),
+                SizedBox(height: 20),
+                Text('History',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                SizedBox(height: 10),
+                ConstrainedBox(
+                  constraints: BoxConstraints(
+                    maxHeight: 200, // Adjust as necessary
+                  ),
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: _conversionHistory.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(_conversionHistory[index]),
+                      );
                     },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                    ),
                   ),
-                  SizedBox(height: 20),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: _convertTemperature,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color.fromARGB(255, 109, 204, 195),
-                        padding: EdgeInsets.symmetric(
-                            horizontal: 50, vertical: 20),
-                        textStyle: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      child: Text('Convert'),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  Text('History',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  SizedBox(height: 10),
-                  Expanded(
-                    child: ListView.builder(
-                      itemCount: _conversionHistory.length,
-                      itemBuilder: (context, index) {
-                        return ListTile(
-                          title: Text(_conversionHistory[index]),
-                        );
-                      },
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
@@ -194,8 +191,7 @@ class _TemperatureConversionScreenState
           _convertedTemperature.isNotEmpty
               ? '$_convertedTemperature ${_isCelsius ? '°F' : '°C'}'
               : '',
-          style: TextStyle(
-              fontSize: 50, fontWeight: FontWeight.bold, color: Colors.teal),
+          style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.teal),
         ),
       ),
     );
